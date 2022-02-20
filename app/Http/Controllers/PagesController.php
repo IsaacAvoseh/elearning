f70301f2b0cbea3a8b031f02dcb927946e9103b2
IsaacAvoseh <avosehisaac0@gmail.com>
2022-02-20 17:04:50 +0100

    optmized

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -210,7 +210,7 @@ class PagesController extends Controller
         // $courses->payments = $payments;
 
   
-         $payments = isset(Payments::where('course_id', $id)->where('user_id', Auth::user()['id'])->first()->id) !== null ? Payments::where('course_id', $id)->where('user_id', Auth::user()['id'])->first()->id : 0;
+         $payments = Payments::where('course_id', $id)->where('user_id', Auth::user()->id)->first();
      
         // dd($payments);
      
